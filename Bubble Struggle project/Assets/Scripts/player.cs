c2f73ef74288873159a0fb033dcd8c7388507605
crazymaltese <34515052+crazymaltese@users.noreply.github.com>
2018-04-01 19:03:05 +0200

    Created more objects and player movments
    Created first script with player movment

---
+++ Bubble Struggle project/Assets/Scripts/player.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class player : MonoBehaviour
+{
+
+    //the speed of the player
+    public float speed = 4f;
+    private float movement = 0f;
+    //to set rigidbody
+    public Rigidbody2D rb;
+
+
+
+    void Update()
+    {
+        //specifies what input you want to get and then multiply with the speed
+        movement = Input.GetAxisRaw("Horizontal") * speed;
+    }
+
+    private void FixedUpdate()
+    {
+        //the actaul movment where the movment then works.
+        // set to be as fast as the users framerate with fixedDeltaTime
+        rb.MovePosition(rb.position + new Vector2(movement * Time.fixedDeltaTime, 0f));
+    }
+
+   
+
+
+
+}
